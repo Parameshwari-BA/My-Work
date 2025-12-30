1b17aa0d9e8df2005810c2cd22f0a6c4a2a67694
Parameshwari-BA <paramukrish28@gmail.com>
2025-12-30 16:52:56 +0530

    add

--- day9-multi-container/web/app.py
+++ day9-multi-container/web/app.py
@@ -5,8 +5,8 @@ redis_client = redis.Redis(host='redis', port=6379, decode_responses=True)
 @app.route('/')
 def home():
        redis_client.incr('hits')
-    count = redis_client.get('hits')
-    return f"Page viewed {count} times!"
+       count = redis_client.get('hits')
+       return f"Page viewed {count} times!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
